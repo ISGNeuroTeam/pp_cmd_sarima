052c56f21277479683e9861847bfceb19d47ec83
Nikita Serditov <nserditov@isgneuro.com>
2023-03-13 15:14:11 +0300

    fixed minor bug with using in postprocessing. locally tested

--- sarima/sarima.py
+++ sarima/sarima.py
@@ -7,6 +7,7 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 def sarima(df: pd.DataFrame, p: int, d: int, q: int, sp: int, sd: int, sq: int, m, steps: int) -> pd.DataFrame:
     order = (p, d, q)
     seasonal_order = (sp, sd, sq, m)
+    df = df.set_index('Date')
     model = SARIMAX(df, order=order, seasonal_order=seasonal_order)
     model_fit = model.fit()
     predictions = model_fit.get_forecast(steps=steps)
